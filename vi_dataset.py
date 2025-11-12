9660e1f56511b63a17126e2d63d854ddfad31a2e
Lochana Marasinghe <lochanatharanganie@gmail.com>
2025-11-12 10:24:11 -0800

    added lengths of the valid numbers to the model

--- vi_dataset.py
+++ vi_dataset.py
@@ -8,12 +8,13 @@ import torch
 from torch.utils.data import Dataset
 
 class VIDataset(Dataset):
-    def __init__(self, X, y):
+    def __init__(self, X, y, mask_lengths):
         self.X = torch.tensor(X, dtype=torch.float32)
         self.y = torch.tensor(y, dtype=torch.float32)
+        self.lengths = torch.tensor(mask_lengths, dtype=torch.long)
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, idx):
-        return self.X[idx], self.y[idx]
+        return self.X[idx], self.y[idx], self.lengths[idx]
