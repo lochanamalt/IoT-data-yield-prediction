7022ca1dbb74bef9e34f09ad502cd3e1707324fb
Lochana Marasinghe <lochanatharanganie@gmail.com>
2026-01-05 12:30:27 -0800

    refactored plotting graphs

--- plots.py
+++ plots.py
@@ -1,37 +1,48 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import datetime as dt
 
 START_DAY = 122 # May 1st in gregorian days
+
 
 def plot_r2(r2_list, total_n_days):
     x = np.arange(START_DAY, START_DAY + total_n_days)
     max_idx = np.argmax(r2_list)
     max_x = x[max_idx]
     max_y = r2_list[max_idx]
-    max_y_rounded = round(max_y, 2)
+
     r2_copy = np.delete(r2_list, max_idx)  # remove max
     second_max_val = np.max(r2_copy)
     second_max_idx = np.where(r2_list == second_max_val)[0][0]
     second_max_x = x[second_max_idx]
-    second_max_y_rounded = round(second_max_val, 2)
+
     plt.figure(figsize=(12, 4))
-    plt.axvline(x=143, color='gray', linestyle='--', linewidth=1,
-                label='Heading date')
+    # plt.axvline(x=143, color='gray', linestyle='--', linewidth=1,
+    #             label='Heading date')
+    plt.axvspan(133,152,color='gray',alpha=0.3,label='Heading window')
+
+
+    plt.xlabel("Day (Gregorian days)")
     plt.plot(x, r2_list, marker='o')
-    plt.scatter(max_x, max_y, color='red', s=80, zorder=5, label="Max Value")
-    plt.text(max_x, max_y, f"  max={max_y_rounded} at day {max_x}", color='red', fontsize=10, va='bottom')
-    plt.scatter(second_max_x, second_max_val, color='red', s=80, zorder=5, label="Second Max Value")
-    plt.text(second_max_x, second_max_val, f"  max={second_max_y_rounded} at day {second_max_x}", color='red',
-             fontsize=10, va='bottom')
+    plt.scatter(max_x, max_y, color='red', s=80, zorder=5)
+    plt.text(max_x, max_y, f" {max_y:.4f} at day {max_x}", color='red', fontsize=10, va='bottom')
+    # plt.scatter(second_max_x, second_max_val, color='red', s=80, zorder=5)
+    # plt.text(second_max_x, second_max_val, f"  {second_max_val:.4f} at day {second_max_x}", color='red',
+    #          fontsize=10, va='bottom')
     plt.grid(True, which='both', linestyle='--', linewidth=0.6, alpha=0.7)
-    plt.xlabel("Upto Days")
+
+
     plt.ylabel("Test R²")
-    plt.title("Test R² over days")
+    # plt.title("Test R² over days")
+    tick_indices = np.arange(0, len(x), 2)
+    tick_values = x[tick_indices]
+
+    plt.xticks(ticks=tick_values, ha='right')
+
     plt.grid(True)
     plt.legend()
     plt.tight_layout()
     plt.show()
-
 
 def plot_mse(mse_list, total_n_days):
     x = np.arange(START_DAY, START_DAY + total_n_days)
@@ -68,8 +79,10 @@ def plot_mse(mse_list, total_n_days):
     plt.tight_layout()
     plt.show()
 
-def plot_predicted_yield(total_n_days: int, y_pred_list, actual_yield: float):
+def plot_predicted_yield(total_n_days: int, y_pred_list, actual_yield: float, heading_date_for_plot: int, plot_name: str):
     x = np.arange(START_DAY, START_DAY + total_n_days)
+    date_labels = [(dt.date(2024, 1, 1) + dt.timedelta(days=int(day) - 1)).strftime("%b %d")
+                   for day in x]
 
     differences = np.abs(np.array(y_pred_list) - actual_yield)
     closest_idx = np.argmin(differences)
@@ -77,22 +90,25 @@ def plot_predicted_yield(total_n_days: int, y_pred_list, actual_yield: float):
     closest_day = x[closest_idx]
     closest_pred = y_pred_list[closest_idx]
 
-
     plt.figure(figsize=(8, 4))
     plt.plot(x, y_pred_list, marker='o', label='Predicted Yield')
 
     plt.axhline(actual_yield, linestyle='--', label=f'Actual Yield ({actual_yield:.2f} kg/m²)', color='r')
-    plt.scatter(closest_day, closest_pred, s=120, edgecolors='black',
-                label='Closest Predicted Yield', zorder=5)
+    # plt.scatter(closest_day, closest_pred, s=120, edgecolors='black',
+    #             label=f'Closest Predicted Yield ({closest_pred:.2f} kg/m²) at day {closest_day}', zorder=5)
+
+    plt.axvline(x=heading_date_for_plot, color='gray', linestyle='--', linewidth=1,
+                label='Heading date')
 
     tick_indices = np.arange(0, len(x), 2)  # every 6th point
     tick_values = x[tick_indices]
     tick_labels = [date_labels[i] for i in tick_indices]
 
-    plt.xlabel("Day (Gregorian days)")
+    plt.xlabel("Date")
     plt.ylabel("Predicted yield (kg/m²)")
+    plt.xticks(ticks=tick_values, labels=tick_labels, rotation=45, ha='right')
 
-    plt.title("Predicted Yield Over Days vs Actual Yield")
+    plt.title(f"Predicted yield over days vs actual yield for plot {plot_name}")
     plt.grid(True)
     plt.legend()
     plt.tight_layout()
